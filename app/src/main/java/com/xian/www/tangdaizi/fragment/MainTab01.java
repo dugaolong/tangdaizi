d8f2dbf2afeb0822bcee379d66f4528df3a01dab
dugaolong <1872387570@qq.com>
2017-09-16 19:51:50 +0800

    给的图

--- app/src/main/java/com/xian/www/tangdaizi/fragment/MainTab01.java
+++ app/src/main/java/com/xian/www/tangdaizi/fragment/MainTab01.java
@@ -5,12 +5,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.xian.www.tangdaizi.R;
 
-public class MainTab01 extends Fragment
+public class MainTab01 extends Fragment implements View.OnClickListener
 {
+
+	/**
+	 * 底部四个按钮
+	 */
+	private RelativeLayout daminggong;
+	private RelativeLayout shi;
+	private RelativeLayout xi;
+	private RelativeLayout datang;
+	private RelativeLayout dayanta;
+	private RelativeLayout yi;
+	private RelativeLayout ke;
+	private RelativeLayout huaqingchi;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -18,7 +31,32 @@ public class MainTab01 extends Fragment
 		View view = inflater.inflate(R.layout.main_tab_01, container, false);
 		TextView textView = (TextView) view.findViewById(R.id.title_text);
 		textView.setText("首页");
+		daminggong = (RelativeLayout) view.findViewById(R.id.daminggong);
+		shi= (RelativeLayout) view.findViewById(R.id.shi);
+		xi = (RelativeLayout) view.findViewById(R.id.xi);
+		datang = (RelativeLayout) view.findViewById(R.id.daminggong);
+		dayanta = (RelativeLayout) view.findViewById(R.id.dayanta);
+		yi = (RelativeLayout) view.findViewById(R.id.yi);
+		ke = (RelativeLayout) view.findViewById(R.id.ke);
+		huaqingchi = (RelativeLayout) view.findViewById(R.id.huaqingchi);
+		daminggong.setOnClickListener(this);
+		shi.setOnClickListener(this);
+		xi.setOnClickListener(this);
+		datang.setOnClickListener(this);
+		dayanta.setOnClickListener(this);
+		yi.setOnClickListener(this);
+		ke.setOnClickListener(this);
+		huaqingchi.setOnClickListener(this);
 		return  view;
 	}
 
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()){
+			case R.id.daminggong:
+
+				break;
+		}
+	}
 }
--- app/src/main/java/com/xian/www/tangdaizi/ui/MainActivity.java
+++ app/src/main/java/com/xian/www/tangdaizi/ui/MainActivity.java
@@ -18,12 +18,13 @@ import com.xian.www.tangdaizi.fragment.MainTab04;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener{
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
     private ViewPager mViewPager;
     private FragmentPagerAdapter mAdapter;
     private List<Fragment> mFragments = new ArrayList<Fragment>();
 
+    private final String TAG = "MainActivity";
     /**
      * 底部四个按钮
      */
@@ -32,27 +33,31 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     private LinearLayout mTabBtnAddress;
     private LinearLayout mTabBtnSettings;
 
+    /**
+     * 四个fragment
+     */
+    MainTab01 tab01;
+    MainTab02 tab02;
+    MainTab03 tab03;
+    MainTab04 tab04;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
 
         initView();
-        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager())
-        {
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
 
             @Override
-            public int getCount()
-            {
+            public int getCount() {
                 return mFragments.size();
             }
 
             @Override
-            public Fragment getItem(int arg0)
-            {
+            public Fragment getItem(int arg0) {
                 return mFragments.get(arg0);
             }
         };
@@ -60,32 +65,29 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         mViewPager.setAdapter(mAdapter);
 
 
-        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener()
-        {
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
             private int currentIndex;
 
             @Override
-            public void onPageSelected(int position)
-            {
+            public void onPageSelected(int position) {
                 resetTabBtn();
-                switch (position)
-                {
+                switch (position) {
                     case 0:
                         ((ImageButton) mTabBtnWeixin.findViewById(R.id.btn_tab_bottom_weixin))
-                                .setImageResource(R.drawable.tab_weixin_pressed);
+                                .setImageResource(R.drawable.zhuye2);
                         break;
                     case 1:
                         ((ImageButton) mTabBtnFrd.findViewById(R.id.btn_tab_bottom_friend))
-                                .setImageResource(R.drawable.tab_find_frd_pressed);
+                                .setImageResource(R.drawable.huodong2);
                         break;
                     case 2:
                         ((ImageButton) mTabBtnAddress.findViewById(R.id.btn_tab_bottom_contact))
-                                .setImageResource(R.drawable.tab_address_pressed);
+                                .setImageResource(R.drawable.gongyi2);
                         break;
                     case 3:
                         ((ImageButton) mTabBtnSettings.findViewById(R.id.btn_tab_bottom_setting))
-                                .setImageResource(R.drawable.tab_settings_pressed);
+                                .setImageResource(R.drawable.geren2);
                         break;
                 }
 
@@ -93,33 +95,29 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
             }
 
             @Override
-            public void onPageScrolled(int arg0, float arg1, int arg2)
-            {
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
 
             }
 
             @Override
-            public void onPageScrollStateChanged(int arg0)
-            {
+            public void onPageScrollStateChanged(int arg0) {
             }
         });
 
     }
 
-    protected void resetTabBtn()
-    {
+    protected void resetTabBtn() {
         ((ImageButton) mTabBtnWeixin.findViewById(R.id.btn_tab_bottom_weixin))
-                .setImageResource(R.drawable.tab_weixin_normal);
+                .setImageResource(R.drawable.zhuye1);
         ((ImageButton) mTabBtnFrd.findViewById(R.id.btn_tab_bottom_friend))
-                .setImageResource(R.drawable.tab_find_frd_normal);
+                .setImageResource(R.drawable.huodong1);
         ((ImageButton) mTabBtnAddress.findViewById(R.id.btn_tab_bottom_contact))
-                .setImageResource(R.drawable.tab_address_normal);
+                .setImageResource(R.drawable.gongyi1);
         ((ImageButton) mTabBtnSettings.findViewById(R.id.btn_tab_bottom_setting))
-                .setImageResource(R.drawable.tab_settings_normal);
+                .setImageResource(R.drawable.geren1);
     }
 
-    private void initView()
-    {
+    private void initView() {
 
         mTabBtnWeixin = (LinearLayout) findViewById(R.id.id_tab_bottom_weixin);
         mTabBtnFrd = (LinearLayout) findViewById(R.id.id_tab_bottom_friend);
@@ -130,10 +128,10 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         mTabBtnFrd.setOnClickListener(this);
         mTabBtnAddress.setOnClickListener(this);
         mTabBtnSettings.setOnClickListener(this);
-        MainTab01 tab01 = new MainTab01();
-        MainTab02 tab02 = new MainTab02();
-        MainTab03 tab03 = new MainTab03();
-        MainTab04 tab04 = new MainTab04();
+        tab01 = new MainTab01();
+        tab02 = new MainTab02();
+        tab03 = new MainTab03();
+        tab04 = new MainTab04();
         mFragments.add(tab01);
         mFragments.add(tab02);
         mFragments.add(tab03);
@@ -142,28 +140,78 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        switch (v.getId())
-        {
+        switch (v.getId()) {
             case R.id.id_tab_bottom_weixin:
                 ((ImageButton) mTabBtnWeixin.findViewById(R.id.btn_tab_bottom_weixin))
-                        .setImageResource(R.drawable.tab_weixin_pressed);
+                        .setImageResource(R.drawable.zhuye2);
                 mViewPager.setCurrentItem(0);
                 break;
             case R.id.id_tab_bottom_friend:
                 ((ImageButton) mTabBtnFrd.findViewById(R.id.btn_tab_bottom_friend))
-                        .setImageResource(R.drawable.tab_find_frd_pressed);
+                        .setImageResource(R.drawable.huodong2);
                 mViewPager.setCurrentItem(1);
                 break;
             case R.id.id_tab_bottom_contact:
                 ((ImageButton) mTabBtnAddress.findViewById(R.id.btn_tab_bottom_contact))
-                        .setImageResource(R.drawable.tab_address_pressed);
+                        .setImageResource(R.drawable.gongyi2);
                 mViewPager.setCurrentItem(2);
                 break;
             case R.id.id_tab_bottom_setting:
                 ((ImageButton) mTabBtnSettings.findViewById(R.id.btn_tab_bottom_setting))
-                        .setImageResource(R.drawable.tab_settings_pressed);
+                        .setImageResource(R.drawable.geren2);
                 mViewPager.setCurrentItem(3);
                 break;
         }
     }
+
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        tab04.onActivityResult(requestCode, resultCode, data);
+//
+//    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        FragmentManager fm = getSupportFragmentManager();
+//        int index = requestCode >> 16;
+//        if (index != 0) {
+//            index--;
+//            if (fm.getFragments() == null || index < 0
+//                    || index >= fm.getFragments().size()) {
+//                Log.w(TAG, "Activity result fragment index out of range: 0x"
+//                        + Integer.toHexString(requestCode));
+//                return;
+//            }
+//            Fragment frag = fm.getFragments().get(index);
+//            if (frag == null) {
+//                Log.w(TAG, "Activity result no fragment exists for index: 0x"
+//                        + Integer.toHexString(requestCode));
+//            } else {
+//                handleResult(frag, requestCode, resultCode, data);
+//            }
+//            return;
+//        }
+//    }
+
+//    /**
+//     * 递归调用，对所有子Fragement生效
+//     *
+//     * @param frag
+//     * @param requestCode
+//     * @param resultCode
+//     * @param data
+//     */
+//    private void handleResult(Fragment frag, int requestCode, int resultCode,
+//                              Intent data) {
+//        frag.onActivityResult(requestCode & 0xffff, resultCode, data);
+//        List<Fragment> frags = frag.getChildFragmentManager().getFragments();
+//        if (frags != null) {
+//            for (Fragment f : frags) {
+//                if (f != null)
+//                    handleResult(f, requestCode, resultCode, data);
+//            }
+//        }
+//    }
+
+
 }
